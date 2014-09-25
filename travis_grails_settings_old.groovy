2894ddf0b1fff38adf35743221616cb10efc8bc3
mbohun <martin.bohun@gmail.com>
2014-09-25 19:11:34 +1000

    changed maven repo host from my nectar vm to the official host: ala-wonder.it.csiro.au

--- travis_grails_settings_old.groovy
+++ travis_grails_settings_old.groovy
@@ -1,9 +1,9 @@
 grails.project.dependency.distribution = {
-  remoteRepository(id:"ala-repo-snapshot", url:"http://130.56.249.242/nexus/content/repositories/snapshots") {
+  remoteRepository(id:"ala-repo-snapshot", url:"http://ala-wonder.it.csiro.au/nexus/content/repositories/snapshots") {
     authentication username: System.getenv("TRAVIS_DEPLOY_USERNAME"), password: System.getenv("TRAVIS_DEPLOY_PASSWORD")
   }
 
-  remoteRepository(id:"ala-repo-release",  url:"http://130.56.249.242/nexus/content/repositories/releases") {
+  remoteRepository(id:"ala-repo-release",  url:"http://ala-wonder.it.csiro.au/nexus/content/repositories/releases") {
     authentication username: System.getenv("TRAVIS_DEPLOY_USERNAME"), password: System.getenv("TRAVIS_DEPLOY_PASSWORD")
   }
 }
