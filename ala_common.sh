0938afe53b9c9df5212815d36b7ff32f3b685dbd
Martin Bohun <martin.bohun@gmail.com>
2014-11-07 13:00:38 +1100

    changed ALA_MAVEN_REPO_URL
    because nexus.ala.org.au is down at the moment

--- ala_common.sh
+++ ala_common.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # all ALA projects should use/source this value from this script
-export ALA_MAVEN_REPO_URL="http://nexus.ala.org.au/content/repositories"
+# export ALA_MAVEN_REPO_URL="http://nexus.ala.org.au/content/repositories"
+export ALA_MAVEN_REPO_URL="http://130.56.249.242/nexus/content/repositories"
 
 # we do not need this, if a custom <id> is NOT set (NOT used), and therefore NOT passed to maven deploy:deploy-file mojo in repositoryID
 # maven will look for the "default" <id>remote-repository</id> in ~/.m2/settings.xml
