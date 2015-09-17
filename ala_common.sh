893307bc35afa60209e496d757a54d6d9e95d40e
mbohun <martin.bohun@gmail.com>
2015-09-17 17:29:33 +1000

    added funtions for setting up grails env, and building grails project

--- ala_common.sh
+++ ala_common.sh
@@ -9,3 +9,31 @@ export ALA_MAVEN_REPO_ID="ala-repo"
 
 # deafult groupId for ALA projects
 export ALA_MAVEN_GROUP_ID="au.org.ala"
+
+# default to SNAPSHOT maven repo
+MAVEN_REPO="ala-repo-snapshot";
+
+function travis_grails_setup_env {
+    rm -rf ~/.sdkman
+    curl -s get.gvmtool.net > ~/install_gvm.sh
+    chmod 775 ~/install_gvm.sh
+    ~/install_gvm.sh
+
+    echo "sdkman_auto_answer=true" > ~/.sdkman/etc/config
+    source ~/.sdkman/bin/sdkman-init.sh
+
+    local grails_version
+    grails_version=`grep '^app\.grails\.version=' ./application.properties | sed -e 's/^app\.grails\.version=//g'`
+    sdk install grails $grails_version || true
+
+    mkdir -p ~/.grails
+    wget -q -O ~/.grails/settings.groovy https://raw.githubusercontent.com/AtlasOfLivingAustralia/travis-build-configuration/master/travis_grails_settings_old.groovy
+    grep '^app\.version=' ./application.properties | grep -q "\-SNAPSHOT"
+    if [ "$?" = "1" ]; then
+	MAVEN_REPO="ala-repo-release"
+    fi
+}
+
+function travis_grails_build {
+    grails clean && grails refresh-dependencies --non-interactive && grails prod war --non-interactive && grails prod maven-deploy --repository=$MAVEN_REPO --non-interactive
+}
