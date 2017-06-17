6ce879a435ff2501e4796ee653a93f69aa011d38
Simon Bear <simon.bear@csiro.au>
2017-06-17 15:04:16 +1000

    add support for grails wrapper and add packaging to grails-test, homogenised indentation

--- ala_common.sh
+++ ala_common.sh
@@ -13,54 +13,63 @@ export ALA_MAVEN_REPO_ID="ala-repo"
 # default groupId for ALA projects
 export ALA_MAVEN_GROUP_ID="au.org.ala"
 
+export GRAILS_COMMAND="grails"
+export PACKAGE_COMMAND="war"
+
 # default to SNAPSHOT maven repo
 MAVEN_REPO="ala-repo-snapshot";
 
-function ala_grails_repo_setup {
-    mkdir -p ~/.grails
-    wget -q -O ~/.grails/settings.groovy https://raw.githubusercontent.com/AtlasOfLivingAustralia/travis-build-configuration/master/travis_grails_settings_old.groovy
+function ala_travis_grails_repo_setup {
+	mkdir -p ~/.grails
+	wget -q -O ~/.grails/settings.groovy https://raw.githubusercontent.com/AtlasOfLivingAustralia/travis-build-configuration/master/travis_grails_settings_old.groovy
 
-    # check if this is a grails application OR a plugin
-    # - grails plugins are storing their version number in the WhateverGrailsPlugin.groovy file
-    # - grails applications in the application.properties file
-    local app_version
-    if [ -f ./*GrailsPlugin.groovy ]
-    then
-	app_version=`grep '^\s*def\s*version' *GrailsPlugin.groovy | sed -e 's/^.*= *"//g' | sed -e 's/".*$//g' | tr -d "\r"`
-    else
-	app_version=`grep '^app\.version=' ./application.properties`
-    fi
+	if [ -f ./grailsw ]
+	then
+		GRAILS_COMMAND="./grailsw"
+	fi
 
-    echo $app_version | grep -q "\-SNAPSHOT"
-    if [ "$?" = "1" ]; then
+	# check if this is a grails application OR a plugin
+	# - grails plugins are storing their version number in the WhateverGrailsPlugin.groovy file
+	# - grails applications in the application.properties file
+	local app_version
+	if [ -f ./*GrailsPlugin.groovy ]
+	then
+		app_version=`grep '^\s*def\s*version' *GrailsPlugin.groovy | sed -e 's/^.*= *"//g' | sed -e 's/".*$//g' | tr -d "\r"`
+		PACKAGE_COMMAND="package-plugin"
+	else
+		app_version=`grep '^app\.version=' ./application.properties`
+	fi
+
+	echo $app_version | grep -q "\-SNAPSHOT"
+	if [ "$?" = "1" ]; then
 	MAVEN_REPO="ala-repo-release"
-    fi
+	fi
 
-    # TODO: if we got here and we still do not MAVEN_REPO set we have a problem, report an ERROR
+	# TODO: if we got here and we still do not MAVEN_REPO set we have a problem, report an ERROR
 }
 
 function ala_travis_grails_setup_env {
-    rm -rf ~/.sdkman
-    curl -s http://get.sdkman.io | bash
+	rm -rf ~/.sdkman
+	curl -s http://get.sdkman.io | bash
 
-    echo "sdkman_auto_answer=true" > ~/.sdkman/etc/config
-    source ~/.sdkman/bin/sdkman-init.sh
+	echo "sdkman_auto_answer=true" > ~/.sdkman/etc/config
+	source ~/.sdkman/bin/sdkman-init.sh
 
-    local grails_version
-    grails_version=`grep '^app\.grails\.version=' ./application.properties | sed -e 's/^app\.grails\.version=//g'`
-    sdk install grails $grails_version || true
+	local grails_version
+	grails_version=`grep '^app\.grails\.version=' ./application.properties | sed -e 's/^app\.grails\.version=//g'`
+	sdk install grails $grails_version || true
 
-    ala_grails_repo_setup
+	ala_travis_grails_repo_setup
 }
 
 function ala_travis_grails_build {
-    grails clean && grails refresh-dependencies --non-interactive && grails prod maven-install --non-interactive && travis_retry grails prod maven-deploy --repository=$MAVEN_REPO --non-interactive
+	$GRAILS_COMMAND clean && $GRAILS_COMMAND refresh-dependencies --non-interactive && $GRAILS_COMMAND prod maven-install --non-interactive && travis_retry $GRAILS_COMMAND prod maven-deploy --repository=$MAVEN_REPO --non-interactive
 }
 
 function ala_travis_grails_test {
-    grails refresh-dependencies --non-interactive && grails test-app --non-interactive
+	$GRAILS_COMMAND refresh-dependencies --non-interactive && $GRAILS_COMMAND test-app --non-interactive && $GRAILS_COMMAND prod $PACKAGE_COMMAND --non-interactive
 }
 
 function ala_travis_grails_deploy {
-    [ "${TRAVIS_PULL_REQUEST}" = "false" ] && travis_retry grails prod maven-deploy --repository=$MAVEN_REPO --non-interactive
+	[ "${TRAVIS_PULL_REQUEST}" = "false" ] && travis_retry $GRAILS_COMMAND prod maven-deploy --repository=$MAVEN_REPO --non-interactive
 }
